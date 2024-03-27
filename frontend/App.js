ed2fcacfd770449070d22974d09e244a79c9175c
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-03-27 17:09:44 +0100

    fe: moved view to own folder

--- frontend/App.js
+++ frontend/App.js
@@ -2,14 +2,8 @@ import * as React from "react";
 import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeView from "./views/HomeView";
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createNativeStackNavigator();
 
@@ -17,7 +11,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeView} />
       </Stack.Navigator>
     </NavigationContainer>
   );
