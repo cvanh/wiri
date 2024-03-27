ab445de887a4b13209164758e2793932e06eec4e
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-03-27 16:20:03 +0100

    lil cleanup

--- backend/tests/Feature/producers/ProducerTest.php
+++ backend/tests/Feature/producers/ProducerTest.php
@@ -4,10 +4,7 @@ namespace Tests\Feature\Producers;
 
 use App\Models\Producer;
 use App\Models\User;
-use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
