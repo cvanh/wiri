71b5c68d37b4e477600fa9b7bf5ee62794429b53
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-03-29 11:09:21 +0100

    cleanup of producer model

--- backend/app/Models/Producer.php
+++ backend/app/Models/Producer.php
@@ -5,10 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Producer extends Model 
+class Producer extends Model
 {
     use HasFactory, HasUuids;
     protected $primaryKey = "id";
