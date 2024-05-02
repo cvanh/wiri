a2f35e6614a23a6e2bf93c72a50da64cb3908173
constantijn <constantijn@vanhartesveldt.nl>
2024-05-02 17:29:34 +0200

    wip on products

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory, HasUuids;
+    protected $primaryKey = "id";
+    public $incrementing = false;
+    protected $table = "products";
+
+    protected $fillable = ["name", "description"];
+}
