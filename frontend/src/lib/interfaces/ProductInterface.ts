8d08b7b53733a48e8807ecf1b396e3f031287f85
constantijn <constantijn@vanhartesveldt.nl>
2024-05-07 04:14:20 +0200

    added product interface for product page

---
+++ frontend/src/lib/interfaces/ProductInterface.ts
@@ -0,0 +1,11 @@
+interface ProductInterface {
+  created_at: Date;
+  deleted_at: Date;
+  description: string;
+  id: string;
+  name: string;
+  producer_id: string;
+  updated_at: Date;
+}
+
+export default ProductInterface;
--- frontend/src/screens/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import ApiModel from '../lib/models/WiriModel';
 
@@ -8,15 +8,43 @@ export default function ProductScreen() {
     useEffect(() => {
         async function getProducts() {
             const data = await ApiModel.getProducts()
-            setproducts(data);
+            setproducts(data.data);
         }
         getProducts()
     }, []);
 
     console.log(products)
     return (
-        <View>
+        <View style={style.container}>
             <Text>ProductScreen</Text>
+            {products?.map((product) => (
+                <View style={style.product} key={product.id}>
+                    <Text>{product.name}</Text>
+                    <Text>{product.description}</Text>
+                    <Text>{product.name}</Text>
+                </View>
+            ))}
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        paddingTop: 10,
+        backgroundColor: "#ecf0f1",
+        padding: 8,
+    },
+    title: {
+
+    },
+    product: {
+        // marginTop: 34,
+        margin: 24,
+        fontSize: 18,
+        width: 100,
+        fontWeight: "bold",
+        textAlign: "center",
+    }
+})
