32114c4688571463487e6161253feae32b315e74
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-05-21 13:47:29 +0200

    unfucked issue with migrations after producer to company change

--- backend/database/migrations/2024_03_29_090137_add_producer_owner.php
+++ backend/database/migrations/2024_03_29_090137_add_producer_owner.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('producers', function (Blueprint $table) {
-            Schema::dropColumn('author_id');
+            $table->dropColumn('author_id');
         });
     }
 };
