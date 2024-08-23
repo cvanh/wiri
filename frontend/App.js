8eccfbaac52b6c1877ef49dedcf8cec2ef2a5090
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-08-23 22:36:58 +0200

    create tab bar in app

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import LoginScreen from "./src/screens/LoginScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 import ProductScreen from "./src/screens/product/ProductScreen";
@@ -10,8 +11,11 @@ import ProductCreate from "./src/screens/product/ProductCreate";
 import ProductDetail from "./src/screens/product/ProductDetail";
 import CompanyDetail from "./src/screens/company/CompanyDetail";
 import MapScreen from "./src/screens/MapScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import SearchScreen from "./src/screens/SearchScreen";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
   const [loggedIn, setloggedIn] = useState(false);
@@ -24,19 +28,31 @@ function App() {
     getData();
   }, []);
 
+  function HomeKaas() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
         {!loggedIn ? (
-          <Stack.Screen name="login" component={LoginScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
         ) : (
           <>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Product" component={ProductScreen} />
+            <Stack.Screen
+              name="App"
+              options={{ headerShown: false }}
+              component={HomeKaas}
+            />
             <Stack.Screen name="ProductCreate" component={ProductCreate} />
             <Stack.Screen name="ProductDetail" component={ProductDetail} />
-
             <Stack.Screen name="CompanyDetail" component={CompanyDetail} />
             <Stack.Screen name="Map" component={MapScreen} />
           </>
---
+++ frontend/src/screens/ProfileScreen.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import axiosInstance from '../lib/axiosInterceptor';
+
+const ProfileScreen = () => {
+    const [User, setUser] = useState()
+
+    useEffect(() => {
+        async function GetUser() {
+            const res = await axiosInstance.get(`/api/user`)
+            setUser(res.data)
+        }
+        GetUser()
+    }, [])
+    console.log(User)
+    return (
+        <View>
+            <Text>your profile:</Text>
+            {User && <>
+                <Text>id {User.id}</Text>
+                <Text>email_verified_at {User.email_verified_at}</Text>
+                <Text>updated_at {User.updated_at}</Text>
+                <Text>created_at {User.created_at}</Text>
+            </>}
+        </View>
+    );
+}
+
+
+export default ProfileScreen;
