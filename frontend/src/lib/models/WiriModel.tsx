6ff2b881bef5830986fb90c0e9aa1b5805cb7442
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-08-23 12:20:21 +0200

    fixed wirimodel to use new axios instance

--- frontend/src/lib/models/WiriModel.tsx
+++ frontend/src/lib/models/WiriModel.tsx
@@ -1,37 +1,34 @@
+import axiosInstance from "../axiosInterceptor";
 import CredentailsInterface from "../interfaces/CredentailsInterface";
 import UserInterface from "../interfaces/UserInterface";
 import CredentailsModel from "./CredentailsModel";
 
 class ApiModel {
-    static async getCsrfToken() {
-        await Api().head("/sanctum/csrf-cookie")
-    }
-
     static async getCurrentUser(): Promise<UserInterface> {
-        return await Api().get("/api/user")
+        return await axiosInstance.get("/api/user")
     }
 
     // login and persist jwt
-    static async login(credentials: CredentailsInterface) {
-        await this.getCsrfToken();
-        const loginRes = await Api("/sanctum/token", {
-            email: credentials.email,
-            password: credentials.password,
-            device_name: "wiri app"
-        })
+    // static async login(credentials: CredentailsInterface) {
+    //     await this.getCsrfToken();
+    //     const loginRes = await Api("/sanctum/token", {
+    //         email: credentials.email,
+    //         password: credentials.password,
+    //         device_name: "wiri app"
+    //     })
 
-        if (loginRes.status !== 200 || !loginRes.data) {
-            // TODO implement
-            console.error("error while logging in")
-        }
-        credentials.token = loginRes.data
-        console.log(credentials)
+    //     if (loginRes.status !== 200 || !loginRes.data) {
+    //         // TODO implement
+    //         console.error("error while logging in")
+    //     }
+    //     credentials.token = loginRes.data
+    //     console.log(credentials)
 
-        CredentailsModel.set(credentials)
-    }
+    //     CredentailsModel.set(credentials)
+    // }
 
     static async getProducts() {
-        return await Api().get("/api/product");
+        return await axiosInstance.get("/api/product");
     }
 
 }
