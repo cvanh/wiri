540b366d2292f3a4f95abf914edfec42f7ed974c
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-08-23 12:19:10 +0200

    added interceptor bc i forgot to include it in the merge fuckup

---
+++ frontend/src/lib/axiosInterceptor.tsx
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import CredentailsModel from './models/CredentailsModel'
+
+const axiosInstance = axios.create({
+    baseURL: 'http://192.168.2.8:8080',
+    withCredentials: true,
+    withXSRFToken: true,
+})
+
+axiosInstance.interceptors.request.use(
+    async (config) => {
+        // TODO make shure this only is set on the requests to the api
+        const credentials = await CredentailsModel.get()
+        config.headers.Authorization = `Bearer ${credentials.token}`
+        return config
+    },
+
+)
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        console.log(response.data)
+        return response
+    },
+    (error) => {
+        console.error("request failed", error)
+        if (error.response && error.response.status === 401) {
+            console.log('call the refresh token api here')
+            // Handle 401 error, e.g., redirect to login or refresh token
+        }
+        return Promise.reject(error)
+    },
+)
+
+export default axiosInstance
