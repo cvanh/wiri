8237b1db7a1e70d92c831a0a1faafd349c542ee7
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-08-24 14:29:40 +0200

    add real weed companies to seed

--- backend/database/factories/CompanyFactory.php
+++ backend/database/factories/CompanyFactory.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Database\Factories;
 
@@ -11,6 +13,26 @@ final class CompanyFactory extends Factory
 {
     protected $model = Company::class;
 
+    protected static $companies = [
+        "THRIVE Cannabis Marketplace Downtown",
+        "The Apothecarium",
+        "Medizin",
+        "New Amsterdam Naturals Las Vegas",
+        "Reef Dispensaries - Las Vegas Strip,",
+        "Reef Dispensaries - Las Vegas North",
+        "THRIVE Cannabis Marketplace",
+        "TheDispensaryNV - West Las Vegas",
+        "TheDispensaryNV - Henderson",
+        "The Clinic",
+        "Pisos - The Strip",
+        "BLUM Las Vegas - Decatur",
+        "Kanna Reno",
+        "RISE Carson City",
+        "JardÃn Premium Cannabis Dispensary",
+        "Top Notch THC",
+        "Shango Las Vegas",
+    ];
+
     /**
      * Define the model's default state.
      * @return array<string, mixed>
@@ -21,7 +43,7 @@ final class CompanyFactory extends Factory
             'id' => fake()->uuid(),
             'type' => fake()->randomElement(['producer', 'store']),
             'author_id' => User::factory()->create()->getAttribute('id'),
-            'name' => fake()->name(),
+            'name' => fake()->randomElement(static::$companies),
             'about' => fake()->paragraph(),
             'latitude' => fake()->latitude(51, 53),
             'longitude' => fake()->longitude(4, 7),
